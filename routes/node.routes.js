31b2bd1c4513e5cbefd761d0acd53084572c99ee
Alex Zimin <alex.zimin@hotmail.com>
2021-07-19 10:42:49 +0300

    fix routes

--- routes/node.routes.js
+++ routes/node.routes.js
@@ -7,16 +7,7 @@ router.get('/nodes', nodeController.getNodes);
 router.get('/nodes/root', nodeController.getRootNode);
 router.get('/nodes/:id', nodeController.getNode);
 router.get('/nodes/:id/children', nodeController.getChildNode);
-router.put('/nodes/:id', nodeController.updateNode);
+router.put('/nodes', nodeController.updateNode);
 router.delete('/nodes/:id', nodeController.deleteNode);
 
 export default router;
-
-// router.post('/nodes', nodeController.createNode);
-// router.get('/nodes', nodeController.getNodes);
-// //ToDo
-// //router.get('/nodes/root', nodeController.getNodes);
-// router.get('/nodes/:id', nodeController.getNode);
-// router.get('/nodes/:id/children', nodeController.getChildNode);
-// router.put('/nodes/:id', nodeController.updateNode);
-// router.delete('/nodes/:id', nodeController.deleteNode);
